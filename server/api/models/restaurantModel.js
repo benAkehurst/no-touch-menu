0bce40559dfa788733917441d51deb779ee81dcb
benakehurst <benakehurst@hotmail.com>
2020-05-09 08:16:39 +0100

    feat(restaurant): change user in restaurant model

--- server/api/models/restaurantModel.js
+++ server/api/models/restaurantModel.js
@@ -2,15 +2,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const User = require('./userModel');
-
 const RestaurantSchema = new Schema(
   {
     restaurantName: {
       type: String,
     },
     user: {
-      type: User,
+      type: Object,
     },
     menus: {
       type: Array,
