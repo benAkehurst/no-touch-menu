24c6c0b97e0a0658095575c810705b00f261a07a
benakehurst <benakehurst@hotmail.com>
2020-05-12 12:52:58 +0100

    feat(qr): remove qr code files

--- server/api/routes/qrRoutes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = (app) => {
-  const qrController = require('../controllers/qrController');
-
-  app
-    .route('/api/qr/generate-qr-code/:requesterId')
-    .get(qrController.generate_qr_code);
-};
--- server/middlewares/qrCode.js
+++
@@ -1,5 +0,0 @@
-const createQrCode = () => {
-  return true;
-};
-
-module.exports = { createQrCode: createQrCode };
