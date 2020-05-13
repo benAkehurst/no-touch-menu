513ffc83169481925d6afe11cedc42278142b7f7
benakehurst <benakehurst@hotmail.com>
2020-05-13 15:00:06 +0100

    feat(client): inital commit of gift-card app client

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { PrivateRoute } from './hoc/PrivateRoute/PrivateRoute';
+
+import Layout from './hoc/Layout/Layout';
+import Auth from './Containers/Auth/Auth';
+import Home from './Containers/Home/Home';
+import Admin from './Containers/Admin/Admin';
+import Account from './Containers/Account/Account';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/" component={Auth}></Route>
+            <Route path="/auth" component={Auth}></Route>
+            <Route path="/home" component={Home}></Route>
+            <Route path="/account" component={Account}></Route>
+            <PrivateRoute exact path="/admin" component={Admin} />
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import classes from './Spinner.module.scss';
+
+class Spinner extends Component {
+  /**
+   * Props:
+   * size - MANDATORY PROP: 'small', 'medium' or 'large'
+   */
+
+  render() {
+    return (
+      <div className={[classes.Spinner, classes[this.props.size]].join(' ')}>
+        Loading...
+      </div>
+    );
+  }
+}
+
+export default Spinner;
