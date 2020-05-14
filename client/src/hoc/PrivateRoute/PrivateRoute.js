83c6aa3bf94128e9970bea0e1ff4cb77137cf638
benakehurst <benakehurst@hotmail.com>
2020-05-14 13:39:31 +0100

    feat(client): update how private route checks token

--- client/src/hoc/PrivateRoute/PrivateRoute.js
+++ client/src/hoc/PrivateRoute/PrivateRoute.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import { getAdminStatus } from '../../Helpers/localStorage';
+import axios from '../../axios-connector';
+import helpers from '../../Helpers/localStorage';
+import BASE_URL from '../../Helpers/BASE_URL';
 
 /* eslint-disable */
 export const PrivateRoute = ({ component: Component, ...rest }) => {
-  const token = getUserToken();
+  const token = helpers.getUserToken();
   return (
     <Route
       {...rest}
       render={(props) =>
-        token === 'true' ? (
+        token !== undefined || null ? (
           <Component {...props} />
         ) : (
           <Redirect
