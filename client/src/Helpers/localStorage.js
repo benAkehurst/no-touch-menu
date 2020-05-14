92a3f211f8d056a03f5376af6658bada5fbbd548
benakehurst <benakehurst@hotmail.com>
2020-05-14 11:55:23 +0100

    feat(client): add new local storage methods

--- client/src/Helpers/localStorage.js
+++ client/src/Helpers/localStorage.js
@@ -1,23 +1,43 @@
-export function addAdminStatus(token) {
-  window.localStorage.setItem('admin', token);
+export function addUserToken(token) {
+  window.localStorage.setItem('token', token);
   return true;
 }
 
-export function addId(id) {
-  window.localStorage.setItem('id', id);
+export function addUserId(id) {
+  window.localStorage.setItem('userId', id);
   return true;
 }
 
-export function getAdminStatus() {
-  return window.localStorage.getItem('admin') || null;
+export function addRestaurantId(id) {
+  window.localStorage.setItem('restaurantId', id);
+  return true;
+}
+
+export function addAdminStatus(id) {
+  window.localStorage.setItem('isAdmin');
+  return true;
+}
+
+export function getUserToken() {
+  return window.localStorage.getItem('token') || null;
 }
 
 export function getUserId() {
-  return window.localStorage.getItem('id') || null;
+  return window.localStorage.getItem('userId') || null;
+}
+
+export function getRestaurantId() {
+  return window.localStorage.getItem('restaurantId') || null;
+}
+
+export function getAdminStatus() {
+  return window.localStorage.getItem('isAdmin') || null;
 }
 
 export function clearStorage() {
-  window.localStorage.removeItem('admin');
-  window.localStorage.removeItem('id');
+  window.localStorage.removeItem('token');
+  window.localStorage.removeItem('userId');
+  window.localStorage.removeItem('restaurantId');
+  window.localStorage.removeItem('isAdmin');
   return true;
 }
