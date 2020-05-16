233447904faf4258d542c5a27dca7217c44d9427
benakehurst <benakehurst@hotmail.com>
2020-05-16 19:08:09 +0100

    feat(restaurant): remove stamp comonent

--- client/src/components/Card/Stamp/Stamp.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Stamp from './Stamp';
-
-export default {
-  title: 'Stamp',
-};
-
-export const HasStamp = () => <Stamp hasStamp={true}></Stamp>;
-export const NoStamp = () => <Stamp hasStamp={false}></Stamp>;
