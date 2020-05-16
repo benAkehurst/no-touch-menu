f482aef2aa119d02b80580697e677c38a60cda3b
benakehurst <benakehurst@hotmail.com>
2020-05-16 12:46:50 +0100

    feat(restaurant): method to fetch restaurant data

--- client/src/Containers/Restaurant/Restaurant.js
+++ client/src/Containers/Restaurant/Restaurant.js
@@ -7,11 +7,16 @@ import BASE_URL from '../../Helpers/BASE_URL';
 
 class Restaurant extends Component {
   state = {
+    isLoading: false,
+    isError: false,
+    errorMessage: null,
     isLoggedIn: null,
+    restaurantData: null,
   };
 
-  componentDidMount() {
+  async componentDidMount() {
     this.checkTokenValid();
+    this.fetchResaurantData();
   }
 
   checkTokenValid = () => {
@@ -19,16 +24,32 @@ class Restaurant extends Component {
     if (!token) {
       this.props.history.push({ pathname: '/auth' });
     }
+    this.setState({ isLoading: true });
     axios
       .get(`${BASE_URL}/auth/check-token-valid/${token}`)
       .then((res) => {
         if (res.status === 200) {
           this.setState({ isLoggedIn: true });
+          return true;
         }
       })
       .catch((err) => {
         helpers.clearStorage();
         this.props.history.push({ pathname: '/auth' });
+      });
+  };
+
+  fetchResaurantData = () => {
+    const restaurantId = helpers.getRestaurantId();
+    this.setState({ isLoading: true });
+    axios
+      .get(`${BASE_URL}/restaurant/get-single-restaurant/${restaurantId}`)
+      .then((res) => {
+        console.log(res.data.data);
+        this.setState({ isLoading: false, restaurantData: res.data.data });
+      })
+      .catch((err) => {
+        this.setState({ isLoading: false, isError: true, errorMessage: err });
       });
   };
 
