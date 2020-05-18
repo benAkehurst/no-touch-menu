ea845d013a48907f9948f9575f0ff88e62617aaa
benakehurst <benakehurst@hotmail.com>
2020-05-18 09:58:22 +0100

    feat(banner): logout method working

--- client/src/components/UI/Banner/Banner.js
+++ client/src/components/UI/Banner/Banner.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux/Aux';
+import helpers from '../../../Helpers/localStorage';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
 class Banner extends Component {
+  logout = () => {
+    helpers.clearStorage();
+    window.location.reload();
+  };
+
   showLogo = () => {
     return <div>Logo</div>;
   };
@@ -15,7 +21,9 @@ class Banner extends Component {
     return (
       <div className={'Buttons-wrapper'}>
         <Button color="inherit">Restaurant</Button>
-        <Button color="inherit">Logout</Button>
+        <Button color="inherit" onClick={() => this.logout()}>
+          Logout
+        </Button>
       </div>
     );
   };
@@ -23,17 +31,15 @@ class Banner extends Component {
   render() {
     return (
       <Aux>
-        <header>
-          <AppBar position="static">
-            <Toolbar>
-              <Typography edge="start" variant="h6">
-                {this.props.siteName}
-              </Typography>
-              {this.props.showLogo ? this.showLogo() : null}
-              {this.props.showUserButtons ? this.showUserButtons() : null}
-            </Toolbar>
-          </AppBar>
-        </header>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography edge="start" variant="h6">
+              {this.props.siteName}
+            </Typography>
+            {this.props.showLogo ? this.showLogo() : null}
+            {this.props.showUserButtons ? this.showUserButtons() : null}
+          </Toolbar>
+        </AppBar>
       </Aux>
     );
   }
