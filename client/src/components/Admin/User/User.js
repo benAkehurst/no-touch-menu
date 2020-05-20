6b2790653585c621135657e20d058b125f5c3205
benakehurst <benakehurst@hotmail.com>
2020-05-20 17:06:17 +0100

    feat(user): method to get single user and change admin status

--- client/src/components/Admin/User/User.js
+++ client/src/components/Admin/User/User.js
@@ -10,13 +10,20 @@ import Button from '@material-ui/core/Button';
 import Banner from '../../../components/UI/Banner/Banner';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Uploader from '../../../components/Uploader/Uploader';
+import Checkbox from '@material-ui/core/Checkbox';
 
 class User extends Component {
   state = {
+    chosenUserId: null,
     isLoading: false,
     isError: false,
     allUsers: null,
     allUsersVisable: false,
+    singleUser: null,
+  };
+
+  userIdHandler = (event) => {
+    this.setState({ chosenUserId: event.target.value });
   };
 
   getAllUsers = () => {
@@ -47,6 +54,50 @@ class User extends Component {
       : this.setState({ allUsersVisable: true });
   };
 
+  getSingleUser = () => {
+    let data = {
+      requesterId: helpers.getUserId(),
+      userId: this.state.chosenUserId,
+    };
+    axios
+      .post(`${BASE_URL}/admin/get-single-user`, data)
+      .then((res) => {
+        if (res.data.success) {
+          this.setState({
+            isLoading: false,
+            singleUser: res.data.data,
+          });
+        }
+      })
+      .catch((err) => {
+        helpers.clearStorage();
+        this.props.history.push({ pathName: '/auth' });
+      });
+  };
+
+  handleIsAdminChange = () => {
+    this.setState({ isLoading: true });
+    let data = {
+      requesterId: helpers.getUserId(),
+      userId: this.state.chosenUserId,
+      isAdminValue: this.state.singleUser.isAdmin ? false : true,
+    };
+    axios
+      .post(`${BASE_URL}/admin/change-user-admin-role`, data)
+      .then((res) => {
+        if (res.data.success) {
+          this.setState({
+            isLoading: false,
+            singleUser: res.data.data,
+          });
+        }
+      })
+      .catch((err) => {
+        helpers.clearStorage();
+        this.props.history.push({ pathName: '/auth' });
+      });
+  };
+
   render() {
     return (
       <Aux>
@@ -56,6 +107,7 @@ class User extends Component {
           </div>
         ) : null}
         <ul>
+          {/* Get All Users */}
           <li className={classes.SingleOption}>
             <div className={classes.SingleOptionHeader}>
               <h4>Get All Users</h4>
@@ -92,7 +144,38 @@ class User extends Component {
                 })
               : null}
           </li>
-          <li>Change User Admin Role</li>
+          {/* Change User Admin Role */}
+          <li className={classes.SingleOption}>
+            <div className={classes.SingleOptionHeader}>
+              <h4>Change User Admin Role</h4>
+              <input
+                placeholder={'User ID'}
+                type="text"
+                onChange={this.userIdHandler}
+              ></input>
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={this.getSingleUser}
+                disabled={!this.state.chosenUserId}
+              >
+                Get User
+              </Button>
+            </div>
+            {this.state.singleUser ? (
+              <div className={classes.ChangeAdminStatus}>
+                <label>Is an Admin?</label>
+                <p>{this.state.singleUser.isAdmin ? 'Yes' : 'No'}</p>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  onClick={this.handleIsAdminChange}
+                >
+                  Change Status
+                </Button>
+              </div>
+            ) : null}
+          </li>
           <li>Change User Status</li>
           <li>Get Single User</li>
           <li>Delete Single User</li>
