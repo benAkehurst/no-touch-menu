0c169200c602f6987ba32ca28dd438dcb0a8d902
benakehurst <benakehurst@hotmail.com>
2020-05-20 15:55:02 +0100

    feat(admin): user options building single option wip

--- client/src/components/Admin/User/User.js
+++ client/src/components/Admin/User/User.js
@@ -12,10 +12,97 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import Uploader from '../../../components/Uploader/Uploader';
 
 class User extends Component {
-  state = {};
+  state = {
+    isLoading: false,
+    isError: false,
+    allUsers: null,
+    allUsersVisable: false,
+  };
+
+  getAllUsers = () => {
+    this.setState({ isLoading: true });
+    let data = {
+      userId: helpers.getUserId(),
+    };
+    axios
+      .post(`${BASE_URL}/admin/get-all-users`, data)
+      .then((res) => {
+        if (res.data.success) {
+          this.setState({
+            isLoading: false,
+            allUsers: res.data.data,
+            allUsersVisable: true,
+          });
+        }
+      })
+      .catch((err) => {
+        helpers.clearStorage();
+        this.props.history.push({ pathName: '/auth' });
+      });
+  };
+
+  hideAllUsers = () => {
+    this.state.allUsersVisable
+      ? this.setState({ allUsersVisable: false })
+      : this.setState({ allUsersVisable: true });
+  };
 
   render() {
-    return <Aux>User</Aux>;
+    return (
+      <Aux>
+        {this.state.isLoading ? (
+          <div className={classes.LoadingBg}>
+            <Spinner size={'large'} />
+          </div>
+        ) : null}
+        <ul>
+          <li className={classes.SingleOption}>
+            <div className={classes.SingleOptionHeader}>
+              <h4>Get All Users</h4>
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={this.getAllUsers}
+              >
+                Fetch
+              </Button>
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={this.hideAllUsers}
+                disabled={!this.state.allUsers}
+              >
+                {this.state.allUsersVisable ? 'Hide' : 'Show'}
+              </Button>
+            </div>
+            {this.state.allUsersVisable
+              ? this.state.allUsers.map((user) => {
+                  return (
+                    <div className={classes.SingleListItem} key={user._id}>
+                      <span>{user.name}</span>
+                      <span
+                        onClick={() => this.props.userIdClick(user._id)}
+                        className={classes.Hoverable}
+                      >
+                        {user._id}
+                      </span>
+                      <span>{user.email}</span>
+                    </div>
+                  );
+                })
+              : null}
+          </li>
+          <li>Change User Admin Role</li>
+          <li>Change User Status</li>
+          <li>Get Single User</li>
+          <li>Delete Single User</li>
+          <li>Create New User</li>
+          <li>Reset Password</li>
+          <li>Check User Valid</li>
+          <li></li>
+        </ul>
+      </Aux>
+    );
   }
 }
 
