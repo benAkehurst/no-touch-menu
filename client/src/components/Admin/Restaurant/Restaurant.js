9ce61c5e351cf0ac0dca598cd7388bba42a34a75
benakehurst <benakehurst@hotmail.com>
2020-05-21 10:36:58 +0100

    feat(restaurant): basic imports and scss

--- client/src/components/Admin/Restaurant/Restaurant.js
+++ client/src/components/Admin/Restaurant/Restaurant.js
@@ -3,11 +3,11 @@ import classes from './Restaurant.module.scss';
 import axios from '../../../axios-connector';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import helpers from '../../../Helpers/localStorage';
+import timeDateHelpers from '../../../Helpers/timeAndDate';
 import BASE_URL from '../../../Helpers/BASE_URL';
 
 import Aux from '../../../hoc/Aux/Aux';
 import Button from '@material-ui/core/Button';
-import Banner from '../../../components/UI/Banner/Banner';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Uploader from '../../../components/Uploader/Uploader';
 
