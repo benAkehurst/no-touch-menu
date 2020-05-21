aaa78c1c6e2a9dd6030fee0d69894a9de4762473
benakehurst <benakehurst@hotmail.com>
2020-05-21 10:25:02 +0100

    feat(user): remove unneeded imports

--- client/src/components/Admin/User/User.js
+++ client/src/components/Admin/User/User.js
@@ -8,9 +8,7 @@ import BASE_URL from '../../../Helpers/BASE_URL';
 
 import Aux from '../../../hoc/Aux/Aux';
 import Button from '@material-ui/core/Button';
-import Banner from '../../../components/UI/Banner/Banner';
 import Spinner from '../../../components/UI/Spinner/Spinner';
-import Uploader from '../../../components/Uploader/Uploader';
 
 class User extends Component {
   state = {
