f77442a457a34a86c44b4abe58992561be4387b4
benakehurst <benakehurst@hotmail.com>
2020-05-22 16:02:58 +0100

    feat(menu): basic component scafolding

--- client/src/components/Admin/Menu/Menu.js
+++ client/src/components/Admin/Menu/Menu.js
@@ -3,11 +3,11 @@ import classes from './Menu.module.scss';
 import axios from '../../../axios-connector';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import helpers from '../../../Helpers/localStorage';
+import timeDateHelpers from '../../../Helpers/timeAndDate';
 import BASE_URL from '../../../Helpers/BASE_URL';
 
 import Aux from '../../../hoc/Aux/Aux';
 import Button from '@material-ui/core/Button';
-import Banner from '../../../components/UI/Banner/Banner';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Uploader from '../../../components/Uploader/Uploader';
 
@@ -17,16 +17,25 @@ class Menu extends Component {
   render() {
     return (
       <Aux>
+        {this.state.isLoading ? (
+          <div className={classes.LoadingBg}>
+            <Spinner size={'large'} />
+          </div>
+        ) : null}
         Menu
         <ul>
           <li>View All Menus</li>
+          {/* View All Menus */}
           <li>View Current Menu</li>
+          {/* View Current Menu */}
           <li>View Current Menu QR Code</li>
+          {/* View Current Menu QR Code */}
           <li>Get Menu as PDF</li>
+          {/* Get Menu as PDF */}
           <li>Add Menu to Restaurant</li>
-          <li>Get All Menus</li>
+          {/* Add Menu to Restaurant */}
           <li>Remove Menu From Restaurant</li>
-          <li></li>
+          {/* Remove Menu From Restaurant */}
         </ul>
       </Aux>
     );
