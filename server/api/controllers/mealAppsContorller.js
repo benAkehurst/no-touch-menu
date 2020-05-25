0d556599d1308772e4b4cf9d1a1cd34754708ef1
benakehurst <benakehurst@hotmail.com>
2020-05-25 13:14:45 +0100

    feat(deliveroo): add menu link as an admin

--- server/api/controllers/mealAppsContorller.js
+++ server/api/controllers/mealAppsContorller.js
@@ -66,7 +66,7 @@ exports.add_deliveroo_link_user = async (req, res) => {
         if (err) {
           res.status(400).json({
             success: false,
-            message: 'Error creating new menu',
+            message: 'Error saving Deliveroo Object',
             data: err,
           });
         }
@@ -78,19 +78,97 @@ exports.add_deliveroo_link_user = async (req, res) => {
           if (err) {
             res.status(400).json({
               success: false,
-              message: 'Error adding new menu to restaurant',
+              message: 'Error adding Deliveroo Data',
               data: err,
             });
           } else {
             res.status(201).json({
               success: true,
-              message: 'Menu updated successfully',
+              message: 'Deliveroo data added successfully',
               data: restaurant,
             });
           }
         }
       );
     }
+  }
+};
+
+exports.add_deliveroo_link_admin = async (req, res) => {
+  const requesterId = req.body.requesterId;
+  const restaurantId = req.body.restaurantId;
+  const deliverooLink = req.body.deliverooLink;
+
+  if (!requesterId || requesterId === null) {
+    res.status(400).json({
+      success: false,
+      message: 'Incorrect Request Paramters',
+      data: null,
+    });
+  }
+  let isAdminCheck;
+  await User.findById(requesterId, (err, user) => {
+    if (user === null) {
+      return false;
+    }
+    if (user.isAdmin) {
+      isAdminCheck = user.isAdmin;
+    }
+  });
+
+  if (isAdminCheck) {
+    const createShortLink = await fetch(
+      'https://api-ssl.bitly.com/v4/shorten',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${process.env.BITLY_TOKEN}`,
+          'content-type': 'application/json',
+        },
+        body: [JSON.stringify({ long_url: deliverooLink })],
+      }
+    );
+    const shortenedLink = await createShortLink.json();
+    const qrCode = await generateQRCode(deliverooLink);
+    let newMenu = new Menu({
+      menuPdfLink: deliverooLink,
+      shortUrlLink: shortenedLink.link,
+      qrCodeBase64: qrCode,
+    });
+    newMenu.save((err, menu) => {
+      if (err) {
+        res.status(400).json({
+          success: false,
+          message: 'Error saving Deliveroo Object',
+          data: err,
+        });
+      }
+    });
+    Restaurant.findByIdAndUpdate(
+      restaurantId,
+      { $set: { deliverooObject: newMenu } },
+      (err, restaurant) => {
+        if (err) {
+          res.status(400).json({
+            success: false,
+            message: 'Error adding Deliveroo Data',
+            data: err,
+          });
+        } else {
+          res.status(201).json({
+            success: true,
+            message: 'Deliveroo data added successfully',
+            data: restaurant,
+          });
+        }
+      }
+    );
+  } else {
+    res.status(401).json({
+      success: false,
+      message: 'Not authorized',
+      data: null,
+    });
   }
 };
 
--- server/api/routes/mealAppsRoutes.js
+++ server/api/routes/mealAppsRoutes.js
@@ -4,4 +4,7 @@ module.exports = (app) => {
   app
     .route('/api/mealApps/add_deliveroo_link_user/:token')
     .post(mealAppsContorller.add_deliveroo_link_user);
+  app
+    .route('/api/mealApps/add_deliveroo_link_admin')
+    .post(mealAppsContorller.add_deliveroo_link_admin);
 };
