5b9a6859d6034a4d8905e55f948a5084bd873019
benakehurst <benakehurst@hotmail.com>
2020-05-25 13:23:31 +0100

    feat(deliveroo): get menu as a pdf user

--- server/api/controllers/mealAppsContorller.js
+++ server/api/controllers/mealAppsContorller.js
@@ -7,7 +7,18 @@ const Menu = mongoose.model('Menu');
 const fetch = require('node-fetch');
 const _ = require('lodash');
 const QRCode = require('qrcode');
+const PDFDocument = require('pdfkit');
 
+/**
+ * Adds a deliveroo menu to the restaurant
+ * USER PROCEDURE
+ * POST
+ * param: token
+ * {
+ *  restaurantId: 'string',
+ *  deliverooLink: 'string'
+ * }
+ */
 exports.add_deliveroo_link_user = async (req, res) => {
   const token = req.params.token;
   const restaurantId = req.body.restaurantId;
@@ -94,6 +105,16 @@ exports.add_deliveroo_link_user = async (req, res) => {
   }
 };
 
+/**
+ * Adds a deliveroo menu to the restaurant
+ * USER PROCEDURE
+ * POST
+ * {
+ *  requesterId: 'string',
+ *  restaurantId: 'string',
+ *  deliverooLink: 'string'
+ * }
+ */
 exports.add_deliveroo_link_admin = async (req, res) => {
   const requesterId = req.body.requesterId;
   const restaurantId = req.body.restaurantId;
@@ -169,6 +190,118 @@ exports.add_deliveroo_link_admin = async (req, res) => {
       message: 'Not authorized',
       data: null,
     });
+  }
+};
+
+/**
+ * Gets deliverro menu as pdf
+ * USER PROCEDURE
+ * GET
+ * param: token
+ * param: requesterId
+ */
+exports.get_deliveroo_PDF_user = async (req, res) => {
+  const token = req.params.token;
+  const restaurantId = req.params.restaurantId;
+
+  if (!token || token === null || !restaurantId || restaurantId === null) {
+    res.status(400).json({
+      success: false,
+      message: 'Incorrect Request Paramters',
+      data: null,
+    });
+  }
+
+  let currentRestaurant = await Restaurant.findById(
+    restaurantId,
+    (err, restaurant) => {
+      if (err) {
+        res.status(500).json({
+          success: false,
+          message: 'Error finding restaurent',
+          data: err,
+        });
+      }
+      if (!restaurant) {
+        res.status(400).json({
+          success: false,
+          message: 'Error finding restaurent',
+          data: err,
+        });
+      }
+      return restaurant;
+    }
+  );
+
+  let tokenValid;
+  await middleware
+    .checkToken(token)
+    .then((promiseResponse) => {
+      if (promiseResponse.success) {
+        tokenValid = true;
+      }
+    })
+    .catch((promiseError) => {
+      if (promiseError) {
+        return res.status(500).json({
+          success: false,
+          message: 'Bad Token',
+          data: null,
+        });
+      }
+    });
+  if (tokenValid) {
+    // Config Elements
+    res.writeHead(200, {
+      'Content-Type': 'application/pdf',
+      'Access-Control-Allow-Origin': '*',
+      'Content-Disposition': 'attachment; filename=DELIVEROO_QR_CODE_MENU.pdf',
+    });
+    const font = 'Helvetica-Bold';
+    const explainerText = 'Scan the QR code below to see us on Deliveroo! ';
+    const url = currentRestaurant.deliverooObject.shortUrlLink;
+    const urlNoProtocol = url.replace(/^https?\:\/\//i, '');
+    const subText = `Or ${urlNoProtocol} if the QR code doesn't scan!`;
+
+    // Create a document
+    const doc = new PDFDocument();
+    doc.pipe(res);
+    // Creating PDF File
+    doc
+      // RESTAURANT NAME
+      .font(font)
+      .fontSize(42)
+      .text(currentRestaurant.restaurantName, {
+        align: 'center',
+        valign: 'center',
+        height: 200,
+        width: 465,
+      })
+      // QR EXPLAINER TEXT
+      .font(font)
+      .fontSize(36)
+      .text(explainerText, {
+        align: 'center',
+        valign: 'center',
+        height: 100,
+        width: 465,
+      })
+      // SUB EXPLAINER TEXT
+      .font(font)
+      .fontSize(30)
+      .text(subText, {
+        align: 'center',
+        valign: 'center',
+        height: 100,
+        width: 465,
+      })
+      // QR CODE
+      .image(currentRestaurant.deliverooObject.qrCodeBase64, {
+        width: 400,
+        height: 400,
+      });
+    // Finalize making PDF file
+    doc.end();
   }
 };
 
--- server/api/routes/mealAppsRoutes.js
+++ server/api/routes/mealAppsRoutes.js
@@ -7,4 +7,7 @@ module.exports = (app) => {
   app
     .route('/api/mealApps/add_deliveroo_link_admin')
     .post(mealAppsContorller.add_deliveroo_link_admin);
+  app
+    .route('/api/mealApps/get_deliveroo_PDF_user/:token/:restaurantId')
+    .get(mealAppsContorller.get_deliveroo_PDF_user);
 };
