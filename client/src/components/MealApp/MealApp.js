fe7e092bb276ae6b9a98ea9453a35b683e05d336
benakehurst <benakehurst@hotmail.com>
2020-05-26 11:24:57 +0100

    feat(mealapp): single card colours and basic methods

--- client/src/components/MealApp/MealApp.js
+++ client/src/components/MealApp/MealApp.js
@@ -12,17 +12,65 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 import Uploader from '../../components/Uploader/Uploader';
 
 class MealApp extends Component {
-  state = {};
+  state = {
+    isLoading: false,
+    isError: false,
+    errorMessage: null,
+    successMessage: null,
+    deliverooUrl: null,
+    justEatUrl: null,
+    uberEatsUrl: null,
+    downloadButtonVisable: false,
+  };
+
+  newUrlInputHandler = (key, e) => {
+    switch (key) {
+      case 'deliverooUrl':
+        this.setState({ deliverooUrl: e.target.value });
+        break;
+      case 'justEatUrl':
+        this.setState({ justEatUrl: e.target.value });
+        break;
+      case 'uberEatsUrl':
+        this.setState({ uberEatsUrl: e.target.value });
+        break;
+      default:
+        break;
+    }
+  };
+
+  saveLinkButtonHandler = (key) => {
+    let mealAppSerice = key;
+    let isAdmin = helpers.getAdminStatus() ? true : false;
+
+    if (isAdmin) {
+      // api calls as admin
+    } else {
+      // api calls as user
+    }
+  };
+
   render() {
+    const spinner = <Spinner size={'large'} />;
     return (
       <Aux>
-        <div>
-          <h1>Meal Apps</h1>
+        <div
+          className={[
+            classes.SingleCard,
+            classes[this.props.deliveryAppColor],
+          ].join(' ')}
+        >
           <h2>{this.props.title}</h2>
+          <div className={classes.CardItem}>Upload</div>
+          <div className={classes.CardItem}>Download</div>
+          {/* I need to be able to:
+            1. add a url link to be uploaded
+            2. see the qr code or download the attached pdf
+          */}
         </div>
       </Aux>
     );
   }
 }
 
-export default MealApp;
+export default withErrorHandler(MealApp, axios);
