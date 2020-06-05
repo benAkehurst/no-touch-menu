a130c8599198ab2d61592928ca79480b71ac5db3
benakehurst <benakehurst@hotmail.com>
2020-06-05 15:35:02 +0100

    feat(client): add new base components

---
+++ client/src/components/Restaurant/MenuOptions/MenuOptions.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class MenuOptions extends Component {
+  render() {
+    return <div>MenuOptions</div>;
+  }
+}
+
+export default MenuOptions;
---
+++ client/src/components/Restaurant/RestaurantOptions/RestaurantOptions.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class RestaurantOptions extends Component {
+  render() {
+    return <div>RestaurantOptions</div>;
+  }
+}
+
+export default RestaurantOptions;
