bc952a02d1ffe9dc9057418cc62e08e6cdb6776e
benakehurst <benakehurst@hotmail.com>
2020-06-08 18:59:59 +0100

    fix(app): fix to error in loading component

--- client/src/hoc/withErrorHandler/withErrorHandler.js
+++ client/src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,7 +9,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null,
     };
 
-    componentWillMount() {
+    componentDidlMount() {
       this.reqInterceptor = axios.interceptors.request.use((req) => {
         this.setState({ error: null });
         return req;
