c43e54ecf6e12cf4b0d1c0cca4f8443ec5c18550
benakehurst <benakehurst@hotmail.com>
2020-06-09 07:32:49 +0100

    feat(landing): wip

--- client/src/Containers/Landing/Landing.js
+++ client/src/Containers/Landing/Landing.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import classes from './Landing.module.scss';
 import axios from '../../axios-connector';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
+import data from './data.json';
+
 import Aux from '../../hoc/Aux/Aux';
+import ContactForm from '../../components/ContactForm/ContactForm';
 import Banner from '../../components/UI/Banner/Banner';
 import Button from '@material-ui/core/Button';
 
@@ -20,6 +24,27 @@ class Landing extends Component {
           showLogo={false}
           showLoginButton={true}
         ></Banner>
+        <main className={classes.PageWrapper}>
+          <section className={classes.TopSection}>
+            <p>{data[0].main_text}</p>
+          </section>
+          <section className={classes.MidSection}>
+            <p>{data[1].main_text}</p>
+          </section>
+          <section className={classes.BottomSection}>
+            <ContactForm heading={'Contact Us'}></ContactForm>
+          </section>
+        </main>
+        {/* top section
+          tag line
+        */}
+        {/* mid section
+          sales text
+        */}
+        {/* bottom section
+          about me
+          contact us form
+        */}
       </Aux>
     );
   }
