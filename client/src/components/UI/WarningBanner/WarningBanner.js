8fad799bdc6a32fd1bd275a38d3c4cfbbee6ea3e
benakehurst <benakehurst@hotmail.com>
2020-06-09 12:27:09 +0100

    feat(warning): change component to function

--- client/src/components/UI/WarningBanner/WarningBanner.js
+++ client/src/components/UI/WarningBanner/WarningBanner.js
@@ -1,22 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './WarningBanner.module.scss';
-import axios from '../../../axios-connector';
-import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
-import helpers from '../../../Helpers/localStorage';
-import BASE_URL from '../../../Helpers/BASE_URL';
-
 import Aux from '../../../hoc/Aux/Aux';
 
-class WarningBanner extends Component {
-  render() {
-    return (
-      <Aux>
-        <div className={classes.WarningBannerWrapper}>
-          You have {this.props.freeTrailCount} days left on your free trail
-        </div>
-      </Aux>
-    );
-  }
-}
+const warningBanner = (props) => (
+  <Aux>
+    <div className={classes.WarningBannerWrapper}>
+      You have {props.freeTrailCount} days left on your free trail
+    </div>
+  </Aux>
+);
 
-export default withErrorHandler(WarningBanner, axios);
+export default warningBanner;
