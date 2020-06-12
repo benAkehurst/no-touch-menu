abf87edb85e850c7a3779d23c3e6e5d3d8b12df3
benakehurst <benakehurst@hotmail.com>
2020-06-12 18:45:45 +0100

    feat(regform): basic breakdown of the component functionality

--- client/src/components/RegisterForm/RegisterForm.js
+++ client/src/components/RegisterForm/RegisterForm.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
+import classes from './RegisterForm.module.scss';
+import axios from '../../axios-connector';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import BASE_URL from '../../Helpers/BASE_URL';
+
+import Aux from '../../hoc/Aux/Aux';
+import Button from '@material-ui/core/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class RegisterForm extends Component {
+  state = {
+    isLoading: false,
+    isError: false,
+    errorMessage: null,
+    isSuccess: false,
+    successMessage: null,
+    isTokenValid: false,
+    tokenValue: null,
+    userName: null,
+    userEmail: null,
+    userPassword: null,
+    restaurantName: null,
+    restaurantTelephone: null,
+  };
+
+  onClickHandler = (key) => {
+    switch (key) {
+      case '':
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  onChangeHandler = (key) => {
+    switch (key) {
+      case '':
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  checkTokenValid = () => {};
+
+  registerNewUser = () => {};
+
   render() {
-    return <div>Register Form</div>;
+    const checkToken = <div>Check Token</div>;
+    const registerUser = <div>Register User</div>;
+
+    return <Aux>{!this.state.isTokenValid ? checkToken : registerUser}</Aux>;
   }
 }
 
-export default RegisterForm;
+export default withErrorHandler(RegisterForm, axios);
