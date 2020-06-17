1052adcc481702fcebc66c5c4bd5f0837e3cd8a6
benakehurst <benakehurst@hotmail.com>
2020-06-17 20:37:45 +0100

    feat(app): update app with new components and routes

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,10 @@ import Landing from './Containers/Landing/Landing';
 import Auth from './Containers/Auth/Auth';
 import Restaurant from './Containers/Restaurant/Restaurant';
 import Admin from './Containers/Admin/Admin';
+import Faq from './Containers/Faq/Faq';
+import Privacy from './Containers/Privacy/Privacy';
+import About from './Containers/About/About';
+import Terms from './Containers/Terms/Terms';
 
 const history = createBrowserHistory();
 
@@ -26,6 +30,10 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={Landing}></Route>
             <Route path="/auth" component={Auth}></Route>
+            <Route path="/faq" component={Faq}></Route>
+            <Route path="/about" component={About}></Route>
+            <Route path="/terms" component={Terms}></Route>
+            <Route path="/privacy" component={Privacy}></Route>
             <PrivateRoute
               path="/restaurant"
               component={Restaurant}
