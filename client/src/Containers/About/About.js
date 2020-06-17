7d20fdf0212ae75ea2c9dd43169c0f7d3f491c8c
benakehurst <benakehurst@hotmail.com>
2020-06-17 21:01:14 +0100

    feat(about): add about us page info

--- client/src/Containers/About/About.js
+++ client/src/Containers/About/About.js
@@ -14,7 +14,23 @@ class About extends Component {
           showLogo={false}
           showLoginButton={false}
         ></Banner>
-        <main>About</main>
+        <main className={classes.AboutWrapper}>
+          <p>
+            Hi! I'm Ben, a web developer based in London and I built this app
+            over the last few weeks to help restaurants and cafes with the issue
+            facing all of us with Covid-19.
+          </p>
+          <p>
+            If you want to talk to me about your restaurant or cafe, then please
+            email me at ben@notouchmenu.app or use the contact form on the home
+            page.
+          </p>
+          <p>
+            If you would like direct access to the app, please put your email
+            into the 'Request Access' box on the home page and we'll send you
+            instructions on how to join the site.
+          </p>
+        </main>
         <Footer></Footer>
       </Aux>
     );
